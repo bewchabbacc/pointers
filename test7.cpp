8998da457f10484077fa866a9f76fae972511124
bewchabbacc <bewchy@gmail.com>
2015-09-25 11:37:35 -0400

    Added HORRIBLE example...

---
+++ test7.cpp
@@ -0,0 +1,21 @@
+#include "helpers.h"
+
+// Supposedly grabs the item pointer by arr, and
+//   moves arr up by one item
+int each(int *arr)
+{
+  int ret = *arr;
+
+  arr += 1;
+  return ret;
+}
+
+int main()
+{
+  int data[] = {32, 54, 33, 76, 4, 26, 76};
+  int *ptr = new int[7];
+
+  ptr = data;
+  for (int i = 0; i < 7; ++i)
+    cout << each(ptr) << " ";
+}
